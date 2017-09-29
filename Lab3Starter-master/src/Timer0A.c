09b7ffbb7f90c4ec85e42b0cc5dcc2bdc08b5c01
jupulidoplaz <31829935+jupulidoplaz@users.noreply.github.com>
2017-09-28 20:10:45 -0500

    comments for Timer0A init and handler

--- Lab3Starter-master/src/Timer0A.c
+++ Lab3Starter-master/src/Timer0A.c
@@ -9,12 +9,13 @@
 #include <stdint.h>
 #include "../inc/tm4c123gh6pm.h"
 
+
 void Timer0A_Init(void){
-	SYSCTL_RCGCTIMER_R |= 0x1;	//Activate timer 0
-	TIMER0_CTL_R = 0x00000000;	//Disable the timer
-	TIMER0_CFG_R = 0x00000000;	//32bit configuration
-	TIMER0_TAMR_R = 0x00000001;	//one shot mode
-	TIMER0_TAILR_R = 800000;		//10ms
+	SYSCTL_RCGCTIMER_R |= 0x1;		//Activate timer 0
+	TIMER0_CTL_R = 0x00000000;		//Disable the timer
+	TIMER0_CFG_R = 0x00000000;		//32bit configuration
+	TIMER0_TAMR_R = 0x00000001;		//one shot mode
+	TIMER0_TAILR_R = 800000;			//10ms
 	TIMER0_TAPR_R = 0;            // 5) bus clock resolution
 	TIMER0_ICR_R = 0x00000001;    // 6) clear timer2A timeout flag
 	TIMER0_IMR_R = 0x00000001;    // 7) arm timeout interrupt
@@ -23,7 +24,7 @@ void Timer0A_Init(void){
 	}
 
 void Timer0A_Handler(void){
-		TIMER0_IMR_R &= ~0x1;						//disarm timout
+		TIMER0_IMR_R &= ~0x1;							//disarm timout
 		TIMER0_ICR_R = TIMER_ICR_TATOCINT;// acknowledge TIMER0A timeout
-		GPIO_PORTB_IM_R  |= 0x1E;
+		GPIO_PORTB_IM_R  |= 0x1E;					//arms buttons
 }
--- Lab3Starter-master/src/Timer0A.h
+++ Lab3Starter-master/src/Timer0A.h
@@ -1,2 +1,27 @@
+
+/* ******** FalloingEdgeInterrupt.h ************** 
+** Enrique Perez-Osborne ehp355
+** Juliana Pulido 
+** Created: 9/20/17
+**EdgeInterrupt Service Routine
+**for when buttons are pressed
+** Lab3
+** TA: Cody Horton
+** Last Modified: 9/28/17
+*/
+
+
+/****************Timer0A_Init***************
+Initializes Timer0A as one-shot timer for 
+debouncing buttons
+inputs: none
+outputs: none
+*/
 void Timer0A_Init(void);
+
+/****************Timer0A_Handler***************
+Handler for Timer0A, re-arms buttons
+inputs: none
+outputs: none
+*/
 void Timer0A_Handler(void);
